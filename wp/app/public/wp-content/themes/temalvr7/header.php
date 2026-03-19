fcd9f910ab82c4b3198d123ce286f12d272e18f7
123eSite <victor@vivacom.com.br>
2026-03-19 15:56:06 -0300

    favicon

--- wp/app/public/wp-content/themes/temalvr7/header.php
+++ wp/app/public/wp-content/themes/temalvr7/header.php
@@ -5,6 +5,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="<?php bloginfo('template_url'); ?>/img/lvr7.jpeg" type="image/x-icon">
 
     <?php wp_head(); ?>
     <!-- Google tag (gtag.js) -->
